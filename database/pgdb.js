eaa8444fcef797794a058ec3622e0f3ac17fc372
Pinkie Aruwani <aruwanip@gmail.com>
2021-11-21 20:47:01 -0500

    Move field mapping to db query

--- database/pgdb.js
+++ database/pgdb.js
@@ -1,3 +1,5 @@
+const humps = require('humps');
+
 module.exports = (pgPool) => {
     return {
         getUser(apiKey) {
@@ -6,7 +8,7 @@ module.exports = (pgPool) => {
                 from users
                 where api_key = $1
             `, [apiKey]).then(res => {
-                return res.rows[0];
+                return humps.camelizeKeys(res.rows[0]);
             });
         }
     };
